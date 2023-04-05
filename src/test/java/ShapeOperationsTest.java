914a305870b885f2d85bff53a700bd8a479d1a49
Jorge Lázaro Ruiz <jorgelazaro2002@hotmail.com>
2023-04-05 19:56:03 +0200

    line break fix (improve readability)

--- src/test/java/ShapeOperationsTest.java
+++ src/test/java/ShapeOperationsTest.java
@@ -9,7 +9,12 @@ import java.util.stream.Stream;
 public class ShapeOperationsTest {
     /* Method 15 */
     public static Stream<Arguments> Rectangles() {
-        return Stream.of(Arguments.arguments(1, 1, 1), Arguments.arguments(2, 2, 4), Arguments.arguments(3, 4, 12), Arguments.arguments(4.22, 5.33, 22.4926), Arguments.arguments(0.5, 0.1, 0.05));
+        return Stream.of(
+                Arguments.arguments(1, 1, 1),
+                Arguments.arguments(2, 2, 4),
+                Arguments.arguments(3, 4, 12),
+                Arguments.arguments(4.22, 5.33, 22.4926),
+                Arguments.arguments(0.5, 0.1, 0.05));
     }
 
     @ParameterizedTest(name = "Area of rectangle with length {0} and width {1}")
