759897485fb47bddedbf7a96b7086ecc26df5cc8
Jorge Lázaro Ruiz <jorgelazaro2002@hotmail.com>
2023-04-05 12:07:29 +0200

    added an exception test for method02

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,9 +8,24 @@ import java.util.stream.Stream;
 
 public class CalculatorTest {
     @ParameterizedTest(name = "Checking if {0} + {1} = {2}")
+
     @MethodSource("positiveNumbers")
     public void method02Test(int a, int b, int expected){
         Assertions.assertEquals(expected, Calculator.addPositiveNumbers(a, b));
+    }
+
+    // Check if the method throws an exception when negative numbers are passed
+    @Test
+    public void method02checkNegativeException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Calculator.addPositiveNumbers(-1, 1);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Calculator.addPositiveNumbers(1, -1);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Calculator.addPositiveNumbers(-1, -1);
+        });
     }
 
     public static Stream<Arguments> positiveNumbers(){
@@ -18,7 +34,8 @@ public class CalculatorTest {
                 Arguments.arguments(1, 2, 3),
                 Arguments.arguments(0, 0, 0),
                 Arguments.arguments(0, 1, 1),
-                Arguments.arguments(1, 0, 1)
+                Arguments.arguments(1, 0, 1),
+                Arguments.arguments(1, 1, 2)
         );
     }
 }
