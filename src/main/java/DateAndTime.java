909dfbcc8078d7aacc96946467fbfbbf723fd72d
laura820 <105165574+laura820@users.noreply.github.com>
2023-04-06 23:49:34 +0200

    Methods 20 implemented and tested

--- src/main/java/DateAndTime.java
+++ src/main/java/DateAndTime.java
@@ -1,3 +1,7 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class DateAndTime {
 
     /**
@@ -18,5 +22,30 @@ public class DateAndTime {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Method 20: Number of days between two dates
+     *
+     * @param date1 first date
+     * @param date2 second date
+     * @return the number of days between the first and the second date
+     * @throws IllegalArgumentException if any date is null
+     * @throws IllegalArgumentException if the second date is prior to the first one
+     * @throws DateTimeException if a date is invalid (day or month are not in the correct range)
+     */
+    public static long numberDays(LocalDate date1, LocalDate date2) {
+        if (date1 == null || date2 == null) {
+            throw new IllegalArgumentException("Dates cannot be null");
+        }
+        if (date2.isBefore(date1)) {
+            throw new IllegalArgumentException("The second date cannot be before the first one");
+        }
+        try {
+            return ChronoUnit.DAYS.between(date1, date2);
+        } catch (DateTimeException e) {
+            throw new DateTimeException("Dates cannot have negative values" + e.getMessage());
+        }
+
     }
 }
--- src/test/java/DateAndTimeTest.java
+++ src/test/java/DateAndTimeTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.util.stream.Stream;
 
 public class DateAndTimeTest {
@@ -30,5 +32,49 @@ public class DateAndTimeTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.isLeapYear(1582));
         Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.isLeapYear(0));
         Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.isLeapYear(-1));
+    }
+
+    /* Method 20 */
+    public static Stream<Arguments> DatesDays() {
+        return Stream.of(
+                Arguments.arguments(LocalDate.of(2022, 6, 2), LocalDate.of(2022, 8, 4), 63),
+                Arguments.arguments(LocalDate.of(2022, 9, 19), LocalDate.of(2023, 1, 19), 122),
+                Arguments.arguments(LocalDate.of(2022, 6, 2), LocalDate.of(2023, 6, 2), 365),
+                Arguments.arguments(LocalDate.of(2023, 5, 1), LocalDate.of(2023, 5, 1), 0),
+                Arguments.arguments(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 2), 1),
+                Arguments.arguments(LocalDate.of(0, 1, 1), LocalDate.of(0, 2, 1), 31),
+                Arguments.arguments(LocalDate.of(-1, 12, 31), LocalDate.of(0, 1, 2), 2)
+        );
+    }
+
+    @ParameterizedTest(name = "Checking number of days")
+    @MethodSource("DatesDays")
+    public void method20Test(LocalDate date1, LocalDate date2, long expected) {
+        Assertions.assertEquals(expected, DateAndTime.numberDays(date1, date2));
+    }
+
+    @Test
+    public void method20TestExceptionsNegatives() {
+        Assertions.assertThrows(DateTimeException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, -1, 1), LocalDate.of(2023, 1, 2)));
+        Assertions.assertThrows(DateTimeException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, 1, -1), LocalDate.of(2023, 1, 2)));
+        Assertions.assertThrows(DateTimeException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, 1, 1), LocalDate.of(2023, -1, 2)));
+        Assertions.assertThrows(DateTimeException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, -2)));
+        Assertions.assertThrows(DateTimeException.class, () -> DateAndTime.numberDays(LocalDate.of(0, 0, 3), LocalDate.of(8, 4, 2)));
+        Assertions.assertThrows(DateTimeException.class, () -> DateAndTime.numberDays(LocalDate.of(0, 9, 0), LocalDate.of(8, 4, 2)));
+
+    }
+
+    @Test
+    public void method20TestExceptionsOrder() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, 4, 3), LocalDate.of(2023, 4, 2)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, 4, 3), LocalDate.of(2023, 2, 2)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.numberDays(LocalDate.of(2023, 4, 3), LocalDate.of(2022, 4, 2)));
+    }
+
+    @Test
+    public void method20TestExceptionsNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.numberDays(null, LocalDate.of(2023, 4, 2)));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.numberDays(LocalDate.of(2022, 4, 3), null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> DateAndTime.numberDays(null, null));
     }
 }
